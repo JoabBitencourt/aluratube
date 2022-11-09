04aeb2ec3c9c10f63735c67d5869958b42526b43
joab bitencourt <joabbitencourt8@gmail.com>
2022-11-08 21:08:31 -0300

    Atualização aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,33 @@
+import React  from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import {CSSReset} from "../src/components/CSSReset"
-import Menu from "../src/components/Menu";
+import Menu from "../src/components/Menu/componentes";
 import {StyledTimeline} from "../src/components/TimeLine"
 
 const HomePage = () => {
   const estiloDaHomePage = { 
     // backgroundColor: "red"
  };
-
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("")
+  // console.log(setValorDoFiltro);
   return (
     <>
       <CSSReset />
       <div style={estiloDaHomePage}>
-        <Menu/>
+        {/* {Prop Drilling} */}
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+
         <Header></Header>
-        <TimeLine playlists={config.playlists}></TimeLine>
+        <TimeLine searchValue={valorDoFiltro} playlists={config.playlists}></TimeLine>
       </div>
     </>
   );
 };
 
 export default HomePage;
+
+
 
 
 const StyledHeader = styled.div`
@@ -32,7 +38,6 @@ const StyledHeader = styled.div`
   }
 
   .user-info {
-    margin-top: 50px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -40,9 +45,17 @@ const StyledHeader = styled.div`
     gap: 16px;
   }
 `;
+
+const StyledBanner = styled.div`
+  background-color: blue;
+  /* background-image: url(${config.bg}); */
+  background: url(${({bg})=> bg}) center/ cover no-repeat;
+  height: 230px;
+`
 const Header = () => {
   return (
     <StyledHeader>
+      <StyledBanner bg={config.bg} />
       {/* <img src="banner"/> */}
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
@@ -55,7 +68,7 @@ const Header = () => {
   );
 };
 
-const TimeLine = (props) => {
+const TimeLine = ({searchValue, ...props}) => {
   const playlistsNames = Object.keys(props.playlists);
   //statement
   //retorno por expressão
@@ -64,12 +77,14 @@ const TimeLine = (props) => {
       {playlistsNames.map((playlistName) => {
         const videos = props.playlists[playlistName];
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
+              {videos.filter((video)=>{
+                return video.title.toLowerCase().includes(searchValue.toLowerCase())
+              }).map((video) => {
                 return (
-                  <a href={video.url}>
+                  <a key={video.url} href={video.url}>
                     <img src={video.thumb} />
                     <span>{video.title}</span>
                   </a>
